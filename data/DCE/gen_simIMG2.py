Fix double precision

data/DCE/gen_simIMG2.py
@@ -104,7 +104,7 @@ def gen_simIMG2(data, idx=None, B1=None, parMap=None) -> tuple[
     print(ID)
 
     # Sensitivity map
-    smap = np.real(data[idx]['smap'].item()).astype(np.float64)
+    smap = np.cdouble(data['smap'].item())
 
     # Add missing fields to the mask if they don't exist
     if 'heart' not in mask.dtype.names:

datasets/gen_DRO.py
@@ -96,7 +96,7 @@ def gen_DRO(data) -> tuple[
     print(ID)
 
     # Sensitivity map
-    smap = np.real(data['smap'].item()).astype(np.float64)
+    smap = np.cdouble(data['smap'].item())
 
     # Add missing fields to the mask if they don't exist
     if 'heart' not in mask.dtype.names: