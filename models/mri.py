86f415ff72c805fa57767ad790a4be78b85560d1
egosche <erik.goesche@fau.de>
2024-04-12 14:05:13 +0200

    cache

--- models/mri.py
+++ models/mri.py
@@ -12,40 +12,45 @@ import torchkbnufft as tkbn
 from typing import Optional, Tuple
 
 
-def fftc(input: torch.Tensor | np.ndarray,
-         axes: Optional[Tuple] = (-2, -1),
-         norm: Optional[str] = 'ortho'):
+def fftc(
+    input: torch.Tensor | np.ndarray,
+    axes: Optional[Tuple] = (-2, -1),
+    norm: Optional[str] = 'ortho',
+):
 
-        if isinstance(input, np.ndarray):
-            tmp = np.fft.ifftshift(input, axes=axes)
-            tmp = np.fft.fftn(tmp, axes=axes, norm=norm)
-            output = np.fft.fftshift(tmp, axes=axes)
+    if isinstance(input, np.ndarray):
+        tmp = np.fft.ifftshift(input, axes=axes)
+        tmp = np.fft.fftn(tmp, axes=axes, norm=norm)
+        output = np.fft.fftshift(tmp, axes=axes)
 
-        elif isinstance(input, torch.Tensor):
-            tmp = torch.fft.ifftshift(input, dim=axes)
-            tmp = torch.fft.fftn(tmp, dim=axes, norm=norm)
-            output = torch.fft.fftshift(tmp, dim=axes)
+    elif isinstance(input, torch.Tensor):
+        tmp = torch.fft.ifftshift(input, dim=axes)
+        tmp = torch.fft.fftn(tmp, dim=axes, norm=norm)
+        output = torch.fft.fftshift(tmp, dim=axes)
 
-        return output
-
-def ifftc(input: torch.Tensor | np.ndarray,
-          axes: Optional[Tuple] = (-2, -1),
-          norm: Optional[str] = 'ortho'):
-
-        if isinstance(input, np.ndarray):
-            tmp = np.fft.ifftshift(input, axes=axes)
-            tmp = np.fft.ifftn(tmp, axes=axes, norm=norm)
-            output = np.fft.fftshift(tmp, axes=axes)
-
-        elif isinstance(input, torch.Tensor):
-            tmp = torch.fft.ifftshift(input, dim=axes)
-            tmp = torch.fft.ifftn(tmp, dim=axes, norm=norm)
-            output = torch.fft.fftshift(tmp, dim=axes)
-
-        return output
+    return output
 
 
-class SenseOp():
+def ifftc(
+    input: torch.Tensor | np.ndarray,
+    axes: Optional[Tuple] = (-2, -1),
+    norm: Optional[str] = 'ortho',
+):
+
+    if isinstance(input, np.ndarray):
+        tmp = np.fft.ifftshift(input, axes=axes)
+        tmp = np.fft.ifftn(tmp, axes=axes, norm=norm)
+        output = np.fft.fftshift(tmp, axes=axes)
+
+    elif isinstance(input, torch.Tensor):
+        tmp = torch.fft.ifftshift(input, dim=axes)
+        tmp = torch.fft.ifftn(tmp, dim=axes, norm=norm)
+        output = torch.fft.fftshift(tmp, dim=axes)
+
+    return output
+
+
+class SenseOp:
     """
     Sensitivity Encoding (SENSE) Operators
 
@@ -54,13 +59,16 @@ class SenseOp():
           Advances in sensitivity encoding with arbitrary k-space trajectories.
           Magn Reson Med (2001).
     """
-    def __init__(self,
-                 coil: torch.Tensor | np.ndarray,
-                 mask: torch.Tensor | np.ndarray,
-                 dcf : Optional[bool] = False,
-                 verbose=False,
-                 normalization=False,
-                 device=torch.device('cpu')):
+
+    def __init__(
+        self,
+        coil: torch.Tensor | np.ndarray,
+        mask: torch.Tensor | np.ndarray,
+        dcf: Optional[bool] = False,
+        verbose=False,
+        normalization=False,
+        device=torch.device('cpu'),
+    ):
         """
         Args:
             coil: [1, N_coil, N_y, N_x]
@@ -90,10 +98,10 @@ class SenseOp():
         assert 2 == mask.shape[-1]
 
         self.N_samples = mask.shape[-2]
-        self.N_spokes  = mask.shape[-3]
-        self.N_frames  = mask.shape[-4]
+        self.N_spokes = mask.shape[-3]
+        self.N_frames = mask.shape[-4]
 
-        base_res = int(self.N_samples//2)
+        base_res = int(self.N_samples // 2)
 
         im_size = [base_res] * 2
         grid_size = [self.N_samples] * 2
@@ -101,8 +109,11 @@ class SenseOp():
         self.ishape = [self.N_frames] + im_size
 
         self.im_size = im_size
+        print(f'im_size: {im_size}\t grid_size: {grid_size}')
         self.NUFFT_FWD = tkbn.KbNufft(im_size=im_size, grid_size=grid_size)
-        self.NUFFT_ADJ = tkbn.KbNufftAdjoint(im_size=im_size, grid_size=grid_size)
+        self.NUFFT_ADJ = tkbn.KbNufftAdjoint(
+            im_size=im_size, grid_size=grid_size
+        )
 
         self.NUFFT_FWD = self.NUFFT_FWD.to(coil.device)
         self.NUFFT_ADJ = self.NUFFT_ADJ.to(coil.device)
@@ -111,9 +122,9 @@ class SenseOp():
 
         if self.normalization:
             if torch.all(nrm_1 == 0):
-                nrm_1 = nrm_1 + 1E-6
+                nrm_1 = nrm_1 + 1e-6
             scale = nrm_0 / nrm_1
-            for _ in range(output_dim-1):
+            for _ in range(output_dim - 1):
                 scale = scale.unsqueeze(1)
         else:
             scale = 1
@@ -140,9 +151,16 @@ class SenseOp():
 
         for t in range(self.N_frames):
 
-            traj_t = torch.reshape(self.mask[..., t, :, :, :], (-1, 2)).transpose(1, 0)
-            imag_t = torch.squeeze(input[..., t, :, :]).unsqueeze(0).unsqueeze(0)
+            traj_t = torch.reshape(
+                self.mask[..., t, :, :, :], (-1, 2)
+            ).transpose(1, 0)
+            imag_t = (
+                torch.squeeze(input[..., t, :, :]).unsqueeze(0).unsqueeze(0)
+            )
 
+            print(
+                f'imag_t: {imag_t.shape}\t traj_t: {traj_t.shape}\t coil: {self.coil.shape}'
+            )
             grid_t = self.NUFFT_FWD(imag_t, traj_t, smaps=self.coil)
 
             if self.verbose:
@@ -179,17 +197,21 @@ class SenseOp():
 
         for t in range(self.N_frames):
 
-            traj_t = torch.reshape(self.mask[..., t, :, :, :], (-1, 2)).transpose(1, 0)
+            traj_t = torch.reshape(
+                self.mask[..., t, :, :, :], (-1, 2)
+            ).transpose(1, 0)
 
             grid_t = input[t]
 
             # density compensation function
             if self.dcf:
-                comp_t = (traj_t[0, ...]**2 + traj_t[1, ...]**2)**0.5 + 1E-5
+                comp_t = (
+                    traj_t[0, ...] ** 2 + traj_t[1, ...] ** 2
+                ) ** 0.5 + 1e-5
                 comp_t = comp_t.reshape(1, -1)
                 # comp_t = tkbn.calc_density_compensation_function(ktraj=traj_t, im_size=self.im_size)
             else:
-                comp_t = 1.
+                comp_t = 1.0
 
             imag_t = self.NUFFT_ADJ(grid_t * comp_t, traj_t, smaps=self.coil)
 
@@ -209,7 +231,8 @@ class SenseOp():
 
         return output
 
-class SenseSp():
+
+class SenseSp:
     """
     Implementation of the SENSE Operator based on SigPy.
     """
